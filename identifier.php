ac320b0022144da4f6e8df76fbe229ae1ec2ed94
c.jackson <jacksoncurt11@gmail.com>
2020-04-23 12:13:08 +0200

    identifier.php

---
+++ identifier.php
@@ -0,0 +1,26 @@
+<?php
+$user_agent = getenv("HTTP_USER_AGENT");
+
+if(strpos($user_agent, "Win") !== FALSE)
+$os = "Windows";
+elseif(strpos($user_agent, "Mac") !== FALSE)
+$os = "Mac";
+elseif(strpos($user_agent, "Lin") !== FALSE)
+$os = "Linux";
+
+if($os === "Windows")
+{
+    $db = new PDO('mysql:host=localhost;dbname=space', 'root', '');
+}
+
+elseif($os === "Mac")
+{
+    $db = new PDO('mysql:host=localhost;dbname=space', 'root', '');
+} 
+
+elseif($os === "Linux")
+{
+    $db = new PDO('mysql:host=localhost;dbname=space', 'phpmyadmin', 'handigital');
+}
+
+?>
