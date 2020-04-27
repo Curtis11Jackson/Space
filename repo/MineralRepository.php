a4d048c57a5e5ef66ff83c8be2cb96b5542fa762
c.jackson <jacksoncurt11@gmail.com>
2020-04-27 11:28:30 +0200

    MAJ des pages

--- repo/MineralRepository.php
+++ repo/MineralRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-class MinerauxRepository
+class MineralRepository
 {
 
   //connexion à la bdd
@@ -17,19 +17,19 @@ class MinerauxRepository
     $this->_db = $db;
   }
 
-  public function add(mineraux $mineraux)
+  public function add(Mineral $mineral)
   {
     //prepare une requete d'ajout de recette
     $request = $this->_db->prepare("INSERT INTO mineraux(nom_min, class_min) VALUES (:nom, :class)");
     //execute la requette avec un tableau d'association
     $request->execute(array(
-      'nom' => $mineraux->nom_min(),
-      'class' => $mineraux->class_min(),
+      'nom' => $mineral->nom_min(),
+      'class' => $mineral->class_min(),
 
     ));
     // On hydrate l'objet afin que son id deviennt l'id qui vient
     //d'être créé
-    $mineraux->hydrate(array(
+    $mineral->hydrate(array(
       'id_min' => $this->_db->lastInsertId()
     ));
   }
@@ -46,32 +46,32 @@ class MinerauxRepository
           $donnees = $request->fetch();
           // on retourne un nouvel objet alien construit
           //avec les donnees récupérer de la BDD
-          return new mineraux($donnees);
+          return new Mineral($donnees);
       }
   }
 
-  public function getListName($id)
+  public function getListName()
   {
-      //on vérifie que le paramètre est bien un id
-      if (is_int($id)) {
-          //on prépare la requete SELECT
-          $request = $this->_db->query("SELECT * FROM mineraux");
-          // On récupère le résultat dans un tableau
-          $donnees = $request->fetch();
-          // on retourne un nouvel objet alien construit
-          //avec les donnees récupérer de la BDD
-          return new mineraux($donnees);
+      $tabloDonnees = [];
+      //execute une requete SELECT qui récupère uniquement les noms de chaque recette
+      $req = $this->_db->query("SELECT * FROM mineraux");
+      //  transformer le résultat en array 
+      while ($donnees = $req->fetch()) {
+        array_push($tabloDonnees, $donnees);
       }
+      // retourner cet array
+      return $tabloDonnees;
   }
 
-  public function update($mineraux)
+
+  public function update($mineral)
   {
       // On prépare la requete afin de modifier un alien dans
       // la BDD, puis on execute en injectant
       // l'attribut $_nom de l'objet $alien
       $request = $this->_db->prepare("UPDATE mineraux SET nom_mineraux = :nom");
       $request->execute(array(
-          'nom' => $mineraux->nom_min()
+          'nom' => $mineral->nom_min()
       ));
   }
 
