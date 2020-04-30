c1c91801bf58a72d982b50bf8780a3df42d4d87c
c.jackson <jacksoncurt11@gmail.com>
2020-04-30 10:20:54 +0200

    Mode responsive toggler white

--- vue/header.php
+++ vue/header.php
@@ -31,7 +31,7 @@
 
   <nav class="navbar navbar-expand-lg navbar-light" style="background-color:black;">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon" style="color:white;"></span>
+    <span class="navbar-toggler-icon" style="background-color:white;"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
