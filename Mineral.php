e67f6eebc7c1500af4dce42e49456565883c9b92
c.jackson <jacksoncurt11@gmail.com>
2020-05-07 11:32:40 +0200

    Mineral.php a jour

--- Mineral.php
+++ Mineral.php
@@ -1,26 +1,4 @@
 <?php
-
-
-
-?>
-
-
-<h1 style="margin-top: 19vw;">mineral</h1>
-
-<form>
-  <div class="row" style="margin-top: 2vw;">
-    <div class="col">
-      <input type="text" id='name' style="margin-left: 3vw; width: 40vw" class="form-control" placeholder="mineral name" >
-    </div>
-    <div class="col">
-      <input type="text" id='mineral' name='classification' style="margin-left: 3vw; margin-right: 3vw; width: 40vw"class="form-control" placeholder="mineral classification">
-    </div>
-
-    <button type="submit" style ="margin-right: 3vw;"class="btn btn-primary">Add mineral</button>
-  </div>
-</form>
-
-<?php
 class Mineral
 {
   private $_id_min;
