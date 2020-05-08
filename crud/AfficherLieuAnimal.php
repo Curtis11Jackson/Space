9e56bb9fb72929076b0ae42634eb6ea130547caa
jnoel69 <jnchambe@gmail.com>
2020-05-08 11:40:09 +0200

    debut du inner join

---
+++ crud/AfficherLieuAnimal.php
@@ -0,0 +1,41 @@
+<?php
+require '../identifier.php';
+require '../chargeauto.php';
+require '../vue/header.php';
+
+?>
+
+<h1>This is list of Place of Animals</h1>
+
+<?php
+
+// $monSituer = new Situer();
+
+$monSituerRepo = new SituerRepository($db);
+
+$tableauLieuAnimo = $monSituerRepo->Situation();
+
+foreach ($tableauLieuAnimo as $Animols) {
+    echo " <article>
+                <h3>" . $Animols['nom_ani'] . "</h3>
+                <p> Numéro id : ". $Animols['id_ani'] . "</p>
+                <p> Race : ". $Animols['race_ani'] . "</p> 
+                <p> Caractère : ". $Animols['carac_ani'] ."</p>
+                <p> Alimentation : " . $Animols['alim_ani'] . "</p>
+                <p> Force : " . $Animols['force_ani'] . "</p>
+                <p> Lieu : " . $Animols['fk_lieu'] . "</p>
+<p style='width:15%;'><a href='DeleteAnimal.php?id=".$Animols['id_ani']."' style='font-size:20px;'>Delete this Animal</a></p>
+<p style='width:15%;'><a href='UpdateAnimal.php?id=".$Animols['id_ani']."
+&nom=".$Animols['nom_ani']."&race=".$Animols['race_ani']."
+&carac=".$Animols['carac_ani']."&alim=".$Animols['alim_ani']."
+&force=".$Animols['force_ani']."&lieu=".$Animols['fk_lieu']."
+' style='font-size:20px;'>Update this Animal</a></p>
+</article>";
+}
+
+
+?>
+
+<?php
+require '../vue/footer.php';
+?>
--- repo/SituerRepository.php
+++ repo/SituerRepository.php
@@ -16,19 +16,18 @@ class SituerRepository
     $this->_db = $db;
   }
 
-    //création du 'JOIUN'
-    public function Situation($id)
+    //création du 'JOIN'
+    public function Situation()
     {
-        //on vérifie que le paramètre est bien un id
-        if (is_int($id)) {
             //on prépare la requete SELECT
-            $request = $this->_db->query("SELECT * FROM ");
+            $request = $this->_db->query("SELECT * FROM Animal
+            INNER JOIN Lieu
+            ON Animal.fk_lieu = Lieu.id_lieu");
             // On récupère le résultat dans un tableau
             $donnees = $request->fetch();
             // on retourne un nouvel objet alien construit
             //avec les donnees récupérer de la BDD
             return new Situer($donnees);
-        }
     }
 
 }
