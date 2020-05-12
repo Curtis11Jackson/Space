56f04e318fe82e338c7c0cc0de4e7e3ddfcad9b8
laurent <laurent.moriceau@gmail.com>
2020-05-12 10:58:26 +0200

    center image

--- crud/AfficherListeLieu.php
+++ crud/AfficherListeLieu.php
@@ -16,15 +16,17 @@ echo "<h3> Number of Places : " .$countLieu[0]. "</h3>";
 $tableauLieu = $monLieuRepo->getListName();
 
 foreach ($tableauLieu as $Lieux) {
+
+
     echo " <article>
                 <h3>" . $Lieux['nom_lieu'] . "</h3>
-                <img src=../images/". $Lieux['img_lieu']." width='320' height='200' class='img-fluid' "."</img>
+                <img style='display:block; margin-left:auto; margin-right:auto;' src=../images/". $Lieux['img_lieu']." width='320' height='200' ' "." </img>
                 <p> Numéro id : ". $Lieux['id_lieu'] . "</p>
                 <p> Topographie Lieu : ". $Lieux['topo_lieu'] . "</p>
                 <p> Position : ". $Lieux['gps_lieu'] ."</p>
                 <p style='width:15%;'><a href='DeleteLieu.php?id=".$Lieux['id_lieu']."' style='font-size:20px;'>Delete this Place</a></p>
                 <p style='width:15%;'><a href='UpdateLieu.php?id=".$Lieux['id_lieu']."
-                &nom=".$Lieux['nom_lieu']."&img=".$Lieux['img_lieu']."&topo=".$Lieux['topo_lieu']."
+                &nom=".$Lieux['nom_lieu']."&topo=".$Lieux['topo_lieu']."
                 &gps=".$Lieux['gps_lieu']."' style='font-size:20px;'>Update this Place</a></p>
         </article>";
 }
