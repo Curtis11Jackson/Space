092d6d1ca1654fc41a2a9708fed42be757b03575
c.jackson <jacksoncurt11@gmail.com>
2020-05-12 11:24:30 +0200

    Tools

--- crud/AjouterOutil.php
+++ crud/AjouterOutil.php
@@ -9,15 +9,24 @@ require '../vue/header.php';
 	<h1 style="margin-bottom:4vh">Add tools</h1>
 	</div>
 
-<form method=POST action=ScriptAjoutOutil.php>
-  <div class="row">
-    <div class="col">
-      <input type="text" id="nom" name="nom" class="form-control" placeholder="Tool name">
-    </div>
-    <div class="col">
-      <input type="text" id="role" name="role" class="form-control" placeholder="Role">
-    </div>
-   <button type="submit" class="btn btn-primary">Add a tool</button>
+<form class="w-75" method="POST" action="ScriptAjoutOutil.php" style="margin:auto;">
+		 <div class="form-group">
+    <label for="nom">Tool Name</label>
+    <input type="text" id="nom" name="nom" class="form-control" placeholder="Tool name">
+  </div>
+  <div class="form-group">
+    <label for="img">Use of tool</label>
+    <input type="text" id="role" name="role" class="form-control" placeholder="Usage">
+  </div>
+  <div class="form-group">
+  <label for="inputState">State</label>
+      <select id="inputState" class="form-control">
+        <option selected>Choose what this tool looks for</option>
+        <option>...</option>
+        <option>...</option>
+        <option>...</option>
+        <option>...</option>
+      </select>
   </div>
 </form>
 
