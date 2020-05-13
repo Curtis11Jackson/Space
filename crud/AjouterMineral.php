01f4289dd2bcb81d3a3184d7805513ef52742ffd
c.jackson <jacksoncurt11@gmail.com>
2020-05-13 09:40:59 +0200

    AjouterMineral avec dropdown

--- crud/AjouterMineral.php
+++ crud/AjouterMineral.php
@@ -1,15 +1,44 @@
 <?php
-
-
+require '../identifier.php';
+require '../chargeauto.php';
 require '../vue/header.php';
 ?>
+<?php
+$monLieuRepo = new LieuRepository($db);
 
+$tabloLieu = $monLieuRepo->getListName();
+?>
 
 <div class="banner">
 	<h1 style="margin-bottom:4vh">Add minerals</h1>
-	</div>
+  </div>
+  
+  <form class="w-75" method="POST" action="ScriptAjoutMineral.php" style="margin:auto;">
+		 <div class="form-group">
+    <label for="nom">Mineral's Name</label>
+    <input type="text" id="nom" name="nom" class="form-control" placeholder="Mineral's name">
+  </div>
+  <div class="form-group">
+    <label for="img">Use of tool</label>
+    <input type="text" id="class" name="class" class="form-control" placeholder="Mineral's Class">
+  </div>
+  <div class="form-group">
+  <label for="inputState">Place where the mineral is</label>
+      <select id="inputState" class="form-control">
 
-<form method=POST action=ScriptAjoutMineral.php>
+      <?php foreach ($tabloLieu as $Lieu) {
+    echo "
+        <option>" . $Lieu['nom_lieu'] . "</option>";
+      }
+        ?>
+      </select>
+  </div>
+  <div style="width:12%; border:1px solid black; margin:auto;">
+  <button type="submit" class="btn btn-primary" style="margin:auto;">Add this Mineral</button>
+  </div>
+  </form>
+
+<!-- <form method=POST action=ScriptAjoutMineral.php>
   <div class="row">
     <div class="col">
       <input type="text" id="name" name="nom" class="form-control" placeholder="Mineral's name">
@@ -22,7 +51,9 @@ require '../vue/header.php';
     </div>
     <button type="submit" class="btn btn-primary">Add a mineral</button>
   </div>
-</form>
+</form> -->
+
+
 
 
 <?php
