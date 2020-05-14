2635fad783945f5a57b082cb44aaec33567dac71
laurent <laurent.moriceau@gmail.com>
2020-05-14 10:51:18 +0200

    lolo forever

--- crud/AjouterOutil.php
+++ crud/AjouterOutil.php
@@ -14,17 +14,17 @@ $tabloMineral = $monMineralRepo->getListName();
 	<h1 style="margin-bottom:4vh">Add tools</h1>
 	</div>
 
-<form class="w-75" method="POST" action="ScriptAjoutOutil.php" style="margin:auto;">
-		 <div class="form-group">
-    <label for="nom">Tool Name</label>
+<form class="w-75" method="POST" action="ScriptAjoutOutil.php" style="margin-left:auto; margin-right:auto; display: flex; flex-direction: column;">
+		 <div class="form-group" style='width: 20vw; margin-left:auto; margin-right:auto;'>
+    <label for="nom" style='color:white;font-weight: bold;'>Tool Name</label>
     <input type="text" id="nom" name="nom" class="form-control" placeholder="Tool name">
   </div>
-  <div class="form-group">
-    <label for="img">Use of tool</label>
+  <div class="form-group" style='width: 20vw;margin-left:auto; margin-right:auto;'>
+    <label for="img" style='color:white;font-weight: bold;'>Use of role</label>
     <input type="text" id="role" name="role" class="form-control" placeholder="Usage">
   </div>
-  <div class="form-group">
-  <label for="inputState">Looks for this mineral</label>
+  <div class="form-group" style='width: 20vw;margin-left:auto; margin-right:auto; '>
+  <label for="inputState" style='color:white;font-weight: bold;'>Looks for this mineral</label>
       <select id="inputState" class="form-control">
 
       <?php foreach ($tabloMineral as $Mineral) {
@@ -34,7 +34,7 @@ $tabloMineral = $monMineralRepo->getListName();
         ?>
       </select>
   </div>
-  <div style="width:12%; border:1px solid black; margin:auto;">
+  <div style="width:12%;  margin:auto; margin-top: 5vw;">
   <button type="submit" class="btn btn-primary" style="margin:auto;">Add this tool</button>
   </div>
 </form>
