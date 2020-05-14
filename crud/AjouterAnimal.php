35a73c630fa6fca1300b3183a8ff78e9484275da
jnoel69 <jnchambe@gmail.com>
2020-05-14 10:15:20 +0200

    modif affichage

--- crud/AjouterAnimal.php
+++ crud/AjouterAnimal.php
@@ -13,36 +13,37 @@ $tabloLieu = $monLieuRepo->getListName();
 	<h1 style="margin-bottom:4vh">Add animals</h1>
 	</div>
 
-<form method=POST action=ScriptAjoutAnimal.php>
-  <div class="row">
+<!-- <form method=POST action=ScriptAjoutAnimal.php> -->
+<form class="w-75" method="POST" action="ScriptAjoutAnimal.php" style="margin:auto;">
+  <div class="col">
     <div class="col">
       <input type="text" id="name" name="name" class="form-control" placeholder="Animal name">
-    </div>
+    </div></br>
     <div class="col">
       <input type="text" id="alim" name="alim" class="form-control" placeholder="Alimentation">
-    </div>
+    </div></br>
     <div class="col">
       <input type="text" id="carac" name="carac" class="form-control" placeholder="Caracteristics">
-    </div>
+    </div></br>
     <div class="col">
       <input type="text" id="race" name="race" class="form-control" placeholder="Race">
-    </div>
+    </div></br>
     <div class="col">
       <input type="text" id="force" name="force" class="form-control" placeholder="Force">
-    </div>
+    </div></br>
     <div class="form-group">
-      <label for="inputState">Place where the animal is</label>
-          <select id="inputState" class="form-control" name="lieu">
-          <?php $i=1; 
-                foreach ($tabloLieu as $Lieu) {
-                    echo "<option value=$i>" . $Lieu['nom_lieu'] . "</option>";
-                    $i++;
-                }
-            ?>
-          </select>
-  </div>
+        <label for="inputState">Place where the animal is</label>
+        <select id="inputState" class="form-control" name="lieu">
+        <?php $i=1; 
+              foreach ($tabloLieu as $Lieu) {
+                  echo "<option value=$i>" . $Lieu['nom_lieu'] . "</option>";
+                  $i++;
+              }
+          ?>
+        </select>
+    </div>
     <button type="submit" class="btn btn-primary">Add an animal</button>
-  </div>
+  </div>  
 </form>
 
 
