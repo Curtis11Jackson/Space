b093532d38779cd0b602cc0b9014ead699ff6ccf
c.jackson <jacksoncurt11@gmail.com>
2020-05-14 10:07:14 +0200

    Mise en page

--- crud/AjouterAlien.php
+++ crud/AjouterAlien.php
@@ -8,22 +8,28 @@ require '../vue/header.php';
   <h1 style="margin-bottom:4vh">Add aliens</h1>
 </div>
 
-<form method=POST action=ScriptAjoutAlien.php>
-  <div class="row">
-    <div class="col">
-      <input type="text" id="nom" name="nom" class="form-control" placeholder="Alien name">
-    </div>
-    <div class="col">
-      <input type="text" id="race" name="race" class="form-control" placeholder="Race">
-    </div>
-    <div class="col">
-      <input type="text" id="carac" name="carac" class="form-control" placeholder="Caracteristics">
-    </div>
-    <button type="submit" class="btn btn-primary">Add an alien</button>
+<form class="w-75" method="POST" action="ScriptAjoutAlien.php" style="margin:auto;">
+		 <div class="form-group">
+    <label for="nom" style="color:white; background-color:black;">Alien's Name</label>
+    <input type="text" id="nom" name="nom" class="form-control" placeholder="Give a name to your alien">
   </div>
-</form>
+  <div class="form-group">
+    <label for="race" style="color:white; background-color:black;">Alien's Species</label>
+    <input type="text" id="class" name="race" class="form-control" placeholder="What species is this Alien ?">
+  </div>
+  <div class="form-group">
+    <label for="race" style="color:white; background-color:black;">Alien's Caracteristics</label>
+    <input type="text" id="class" name="carac" class="form-control" placeholder="What are the Alien's caracteristics ?">
+  </div>
+
+  <div style="width:12%;margin:auto;">
+  <button type="submit" class="btn btn-primary" style="margin:auto;">Add this Alien</button>
+  </div>
+  </form>
 
 
 <?php
 require '../vue/footer.php';
 ?>
+
+
