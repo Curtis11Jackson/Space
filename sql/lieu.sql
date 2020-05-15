8f1889d9f88ea60f84c32c893484093def558dfd
c.jackson <jacksoncurt11@gmail.com>
2020-05-15 10:45:09 +0200

    sql lieu

--- sql/lieu.sql
+++ sql/lieu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 10 avr. 2020 à 08:58
+-- Généré le :  ven. 15 mai 2020 à 08:43
 -- Version du serveur :  8.0.18
 -- Version de PHP :  7.4.0
 
@@ -32,19 +32,21 @@ DROP TABLE IF EXISTS `lieu`;
 CREATE TABLE IF NOT EXISTS `lieu` (
   `id_lieu` int(11) NOT NULL AUTO_INCREMENT,
   `nom_lieu` varchar(30) NOT NULL,
-  `topo_lieu` varchar(30) NOT NULL,
-  `GPS_lieu` int(10) NOT NULL,
+  `img_lieu` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `topo_lieu` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `gps_lieu` int(10) NOT NULL,
   PRIMARY KEY (`id_lieu`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `lieu`
 --
 
-INSERT INTO `lieu` (`id_lieu`, `nom_lieu`, `topo_lieu`, `GPS_lieu`) VALUES
-(1, 'Dakara ', 'Desert sec et massif', 123456789),
-(2, 'Celestis', 'Marecageux', 987654321),
-(3, 'Heliopolis', 'Ocean et falaises', 456789123);
+INSERT INTO `lieu` (`id_lieu`, `nom_lieu`, `img_lieu`, `topo_lieu`, `gps_lieu`) VALUES
+(1, 'Dakara ', 'MosEisley.jpg', 'Desert sec et massif          ', 123456789),
+(2, 'Heliopolis', 'Theed.png', 'Ocean et falaises', 456789123),
+(3, 'Tatouine', 'Tatouine.jpg', 'Immense désert', 214748364),
+(4, 'That\'s no star !', 'DeathStar.jpg', 'A metal Planet ', 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
