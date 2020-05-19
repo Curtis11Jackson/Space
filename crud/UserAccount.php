3fff998098f2ea00f7cb491d4b6375c31ec27f02
jnoel69 <jnchambe@gmail.com>
2020-05-19 10:57:23 +0200

    corrections

--- crud/UserAccount.php
+++ crud/UserAccount.php
@@ -24,8 +24,8 @@ foreach ($UserTable as $Userz) {
               <h3>" . $Userz['nom_user'] . "</h3>
               <p> Mail : ". $Userz['mail_user'] . "</p>
               <p> Login : ". $Userz['login_user'] . "</p>
-              <p style='width:15%;'><a href='UpdateUser.php?id=".$UserTable['id_user']."&prenom=".$UserTable['prenom_user']." &nom=".$UserTable['nom_user']." &mail=" .$UserTable['mail_user']. "&login.=" .$UserTable['login_user']. " &password=" .$UserTable['passw_user']."' style='font-size:20px;'>Made a mistake? Click here to make some changes</a></p>
-              <p style='width:15%;'><a href='DeleteUser.php?id=".$UserTable['id_user']."' style='font-size:20px;'>Delete my account</a></p>
+              <p style='width:15%;'><a href='UpdateUser.php?id=".$Userz['id_user']."&prenom=".$Userz['prenom_user']." &nom=".$Userz['nom_user']." &mail=" .$Userz['mail_user']. "&login.=" .$Userz['login_user']. " &password=" .$Userz['passw_user']."' style='font-size:20px;'>Made a mistake? Click here to make some changes</a></p>
+              <p style='width:15%;'><a href='DeleteUser.php?id=".$Userz['id_user']."' style='font-size:20px;'>Delete my account</a></p>
               </article>";
 
 
