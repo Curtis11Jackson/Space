039d602cdf50fb820046c86993befe0b09e55dde
c.jackson <jacksoncurt11@gmail.com>
2020-05-19 12:06:45 +0200

    Page de User existant

---
+++ crud/CheckedAccount.php
@@ -0,0 +1,18 @@
+<?php
+
+require '../identifier.php';
+require '../chargeauto.php';
+require '../vue/header.php';
+
+?>
+
+<h1>Welcome back</h1>
+
+
+
+
+<?php
+
+require '../vue/footer.php';
+
+?>
