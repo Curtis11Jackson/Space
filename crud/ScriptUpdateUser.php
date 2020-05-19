0907d568cc68ca66aaea1464ab25a82d6098ea5c
c.jackson <jacksoncurt11@gmail.com>
2020-05-19 09:56:08 +0200

    Form User Update

---
+++ crud/ScriptUpdateUser.php
@@ -0,0 +1,46 @@
+<?php
+require '../identifier.php';
+require '../chargeauto.php';
+require '../vue/header.php';
+?>
+
+<div class="banner">
+  <h1 style="margin-bottom:4vh">Updated Alien</h1>
+</div>
+
+<?php
+$id = $_POST['id'];
+$prenom =  $_POST['prenom'];
+$nom =  $_POST['nom'];
+$mail = $_POST['mail'];
+$login =  $_POST['login'];
+$password = $_POST['password'];
+
+
+$UserTable = array(
+  "id_user" => $id,
+  "prenom_user" => $prenom,
+  "mail_user" => $mail,
+  "login_user" => $login,
+  "passw_user" => $password,
+);
+
+$myUser = new User($UserTable);
+
+$monUserRepo = new UserRepository($db);
+
+$monUserRepo -> update($myUser);
+
+
+  echo " <article>
+              <p> Your First Name : ". $UserTable['prenom_user'] . "</p>
+              <p> Your Surname : ". $UserTable['nom_user'] . "</p>
+              <p> Your Email : ". $UserTable['mail_user'] . "</p>
+              <p> Your Password  : " . $UserTable['passw_user'] . " </p> 
+      </article>";
+
+
+
+?>
+
+
