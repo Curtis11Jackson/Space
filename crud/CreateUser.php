87067cd00195b7aaf678d93281c58dc30203f5cd
c.jackson <jacksoncurt11@gmail.com>
2020-05-20 10:15:12 +0200

    required form CreateUser

--- crud/CreateUser.php
+++ crud/CreateUser.php
@@ -22,23 +22,23 @@ require '../vue/header.php';
   <form class="w-50" method="POST" action="AddedUser.php" style="margin:auto;">
 		 <div class="form-group">
     <label for="prenom" style="color:white;">Prénom</label>
-    <input  type="text" id="prenom" name="prenom" class="form-control" placeholder="Renseignez votre Prénom">
+    <input  type="text" id="prenom" name="prenom" class="form-control" placeholder="Renseignez votre Prénom" required>
   </div>
   <div class="form-group">
     <label for="nom" style="color:white;">Nom</label>
-    <input type="text" id="nom" name="nom" class="form-control" placeholder="Renseignez votre Nom">
+    <input type="text" id="nom" name="nom" class="form-control" placeholder="Renseignez votre Nom" required>
   </div>
   <div class="form-group">
     <label for="mail" style="color:white;">Email</label>
-    <input type="text" id="mail" name="mail" class="form-control" placeholder="Renseignez votre Email">
+    <input type="text" id="mail" name="mail" class="form-control" placeholder="Renseignez votre Email" required>
   </div>
   <div class="form-group">
     <label for="login" style="color:white;">Login</label>
-    <input type="text" id="login" name="login" class="form-control" placeholder="Renseignez votre Login">
+    <input type="text" id="login" name="login" class="form-control" placeholder="Renseignez votre Login" required>
   </div>
   <div class="form-group">
     <label for="password" style="color:white;">Mot de Passe</label>
-    <input type="password" id="password" name="password" class="form-control" placeholder="Renseignez votre Mot de Passe">
+    <input type="password" id="password" name="password" class="form-control" placeholder="Renseignez votre Mot de Passe" required>
   </div>
   <div style="width:15%; margin:auto;">
   <button type="submit" class="btn btn-primary" style="margin:auto;">Créer mon compte</button>
