f0af7e7f0e452b56834fd9990264f97c44a09a5c
c.jackson <jacksoncurt11@gmail.com>
2020-05-26 11:37:36 +0200

    MAJ

--- crud/CheckedAccount.php
+++ crud/CheckedAccount.php
@@ -24,7 +24,7 @@ if ($donnees2 != false) {
     <h2 style='background-color:red; border:2px white solid; 
     width:30%; text-align:center; margin:auto; margin-top:2vh;'>
    
-    Fisrtname : ".$donnees2[0]['prenom_user']." <br>
+    Firstname : ".$donnees2[0]['prenom_user']." <br>
     Lastname : ".$donnees2[0]['nom_user']."
     </h2></article>";    
 }
@@ -79,11 +79,7 @@ else{
 </div>
 </div>
 
-
-
-
 <?php
-
 require '../vue/footer.php';
 
 ?>
