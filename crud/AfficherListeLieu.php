d7fc7203a51a6305220fd26f4a6506c820db07e5
c.jackson <jacksoncurt11@gmail.com>
2020-05-27 09:28:28 +0200

    Liste Lieu Button

--- crud/AfficherListeLieu.php
+++ crud/AfficherListeLieu.php
@@ -21,13 +21,14 @@ foreach ($tableauLieu as $Lieux) {
     echo " <article>
                 <h3>" . $Lieux['nom_lieu'] . "</h3>
                 <img style='display:block; margin-left:auto; margin-right:auto;' src=../images/". $Lieux['img_lieu']." width='320' height='200' ' "." </img>
-                <p> Numéro id : ". $Lieux['id_lieu'] . "</p>
-                <p> Topographie Lieu : ". $Lieux['topo_lieu'] . "</p>
-                <p> Position : ". $Lieux['gps_lieu'] ."</p>
+                <p style='width:35%; margin:auto;'> Numéro id : ". $Lieux['id_lieu'] . "</p>
+                <p style='width:35%; margin:auto;'> Topographie Lieu : ". $Lieux['topo_lieu'] . "</p>
+                <p style='width:35%; margin:auto; margin-bottom:2%;'> Position : ". $Lieux['gps_lieu'] ."</p>
                 <p style='width:15%;'><a href='DeleteLieu.php?id=".$Lieux['id_lieu']."' style='font-size:20px;'>Delete this Place</a></p>
                 <p style='width:15%;'><a href='UpdateLieu.php?id=".$Lieux['id_lieu']."
                 &nom=".$Lieux['nom_lieu']."&topo=".$Lieux['topo_lieu']."
                 &gps=".$Lieux['gps_lieu']."' style='font-size:20px;'>Update this Place</a></p>
+                <p style='width:15%;'><a href='AjouterComment.php?id=".$Lieux['id_lieu']."' style='font-size:20px;'>Review this Place</a></p>
         </article>";
 }
 ?>
