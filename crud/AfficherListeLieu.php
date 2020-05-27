fee9f641a05aa83d0b6c5769cf56640871f07e16
c.jackson <jacksoncurt11@gmail.com>
2020-05-27 09:38:58 +0200

    Display ListeLieu

--- crud/AfficherListeLieu.php
+++ crud/AfficherListeLieu.php
@@ -6,6 +6,7 @@ require '../vue/header.php';
 
 <h1>The list of Places</h1>
 
+
 <?php
 $monLieuRepo = new LieuRepository($db);
 
@@ -13,26 +14,29 @@ $countLieu = $monLieuRepo->getCountById();
 
 echo "<h3> Number of Places : " .$countLieu[0]. "</h3>";
 
+?>
+<section style="display:flex; flex-wrap: wrap;">
+<?php
 $tableauLieu = $monLieuRepo->getListName();
 
 foreach ($tableauLieu as $Lieux) {
 
 
-    echo " <article>
+    echo " <article style='border:1px solid white; width:50%;'>
                 <h3>" . $Lieux['nom_lieu'] . "</h3>
                 <img style='display:block; margin-left:auto; margin-right:auto;' src=../images/". $Lieux['img_lieu']." width='320' height='200' ' "." </img>
                 <p style='width:35%; margin:auto;'> Numéro id : ". $Lieux['id_lieu'] . "</p>
                 <p style='width:35%; margin:auto;'> Topographie Lieu : ". $Lieux['topo_lieu'] . "</p>
                 <p style='width:35%; margin:auto; margin-bottom:2%;'> Position : ". $Lieux['gps_lieu'] ."</p>
-                <p style='width:15%;'><a href='DeleteLieu.php?id=".$Lieux['id_lieu']."' style='font-size:20px;'>Delete this Place</a></p>
-                <p style='width:15%;'><a href='UpdateLieu.php?id=".$Lieux['id_lieu']."
+                <p style='width:25%;'><a href='DeleteLieu.php?id=".$Lieux['id_lieu']."' style='font-size:20px;'>Delete this Place</a></p>
+                <p style='width:25%;'><a href='UpdateLieu.php?id=".$Lieux['id_lieu']."
                 &nom=".$Lieux['nom_lieu']."&topo=".$Lieux['topo_lieu']."
                 &gps=".$Lieux['gps_lieu']."' style='font-size:20px;'>Update this Place</a></p>
-                <p style='width:15%;'><a href='AjouterComment.php?id=".$Lieux['id_lieu']."' style='font-size:20px;'>Review this Place</a></p>
+                <p style='width:25%;'><a href='AjouterComment.php?id=".$Lieux['id_lieu']."' style='font-size:20px;'>Review this Place</a></p>
         </article>";
 }
 ?>
-
+</section>
 <?php
 
 require '../vue/footer.php';
