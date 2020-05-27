cf57d85fead777c008ee2c6d2441c506d63577a4
c.jackson <jacksoncurt11@gmail.com>
2020-05-27 11:45:21 +0200

    Lable CreateUser

--- crud/CreateUser.php
+++ crud/CreateUser.php
@@ -98,36 +98,32 @@ require '../chargeauto.php';
 	<h1 style="margin-bottom:4vh">Space Exploration</h1>
 </div>
 
-
-
-<div class="parent">
-  <img class="child" src="../images/naut.jpg" style="width:15%; height:10%;">
-</div>
-
-<h2 style="color:white; margin-top:4vh; text-align:center; background-color:black; width:20%; margin:auto;">Create an account</h2>
+<h2 style="color:white; padding-bottom:5px; margin-top:4vh; text-align:center; background-color:black; width:20%; margin:auto; border-radius: 25px;">
+Create an account
+</h2>
   
-  <form class="w-50" method="POST" action="AddedUser.php" style="margin:auto;">
+  <form class="w-25" method="POST" action="AddedUser.php" style="margin:auto; margin-top:2%;">
 		 <div class="form-group">
-    <label for="prenom" style="color:white;">Prénom</label>
+    <label for="prenom"style="background:black;color:white; padding:5px; border-radius: 25px;">Prénom</label>
     <input  type="text" id="prenom" name="prenom" class="form-control" placeholder="Renseignez votre Prénom" required>
   </div>
   <div class="form-group">
-    <label for="nom" style="color:white;">Nom</label>
+    <label for="nom"style="background:black;color:white; padding:5px; border-radius: 25px;">Nom</label>
     <input type="text" id="nom" name="nom" class="form-control" placeholder="Renseignez votre Nom" required>
   </div>
   <div class="form-group">
-    <label for="mail" style="color:white;">Email</label>
+    <label for="mail" style="background:black;color:white; padding:5px; border-radius: 25px;">Email</label>
     <input type="text" id="mail" name="mail" class="form-control" placeholder="Renseignez votre Email" required>
   </div>
   <div class="form-group">
-    <label for="login" style="color:white;">Login</label>
+    <label for="login"style="background:black;color:white; padding:5px; border-radius: 25px;">Login</label>
     <input type="text" id="login" name="login" class="form-control" placeholder="Renseignez votre Login" required>
   </div>
   <div class="form-group">
-    <label for="password" style="color:white;">Mot de Passe</label>
+    <label for="password"style="background:black;color:white; padding:5px; border-radius: 25px;">Mot de Passe</label>
     <input type="password" id="password" name="password" class="form-control" placeholder="Renseignez votre Mot de Passe" required>
   </div>
-  <div style="width:15%; margin:auto;">
+  <div style="width:40%; margin:auto;">
   <button type="submit" class="btn btn-primary" style="margin:auto;">Créer mon compte</button>
   </div>
   </form>
