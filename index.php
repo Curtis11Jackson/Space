6699edf9cb47c8c83a3cf367efda09d3dea63abe
c.jackson <jacksoncurt11@gmail.com>
2020-05-28 09:58:27 +0200

    index.php

--- index.php
+++ index.php
@@ -107,19 +107,19 @@ require 'chargeauto.php';
 
 <h2 style="color:white; margin-top:4vh; text-align:center;">Log In</h2>
 
-<form class="w-50" method="POST" action="crud/CheckAccount.php" style="margin:auto;">
+<form class="w-25" method="POST" action="crud/CheckAccount.php" style="margin:auto;">
   <div class="form-group">
-    <label for="login" style="color:white;">Login</label>
+    <label for="login" style="color:white; font-size:18px;">Login</label>
     <input type="text" id="login" name="login" class="form-control" placeholder="Renseignez votre Login" required>
   </div>
   <div class="form-group">
-    <label for="password" style="color:white;">Mot de Passe</label>
+    <label for="password" style="color:white; font-size:18px;">Mot de Passe</label>
     <input type="password" id="password" name="password" class="form-control" placeholder="Renseignez votre Mot de Passe" required>
   </div>
   <div style="width:15%; margin:auto;">
     <button type="submit" class="btn btn-success" style="margin:auto;">Accept</button>
   </div>
-  <a href="crud/CreateUser.php" style="font-size:16px; margin-top:2%;" class="btn btn-primary">
+  <a href="crud/CreateUser.php" style="font-size:16px; margin-top:3%;" class="btn btn-primary">
     Create an account
   </a>
 </form>
